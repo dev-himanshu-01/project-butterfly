49f95d3731f870c135165e9e8249502d50973d95
dev-himanshu-01 <himanshu01.dev@gmail.com>
2022-12-27 01:33:58 -0800

    navbar v1.1 + structure setup ✅

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Navbar.css"
+// import SNAP_PROFILE from "../Assets/snapchat-profile.webp"
+
+const Navbar = () => {
+    return (
+        <section className='navbar-main'>
+            <ul>
+                <li>HAPPY B'DAY RIVA 🎂</li>
+
+            </ul>
+        </section>
+    )
+}
+
+export default Navbar
